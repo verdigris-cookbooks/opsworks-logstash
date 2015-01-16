7dd7190e7f99b7f740df5d5a4d8f4ca57099d2ac
Andrew Jo <andrewjo@gmail.com>
2015-01-16 11:10:09 -0800

    Fix version constraints

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs and configures Logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'logstash', '~0.11.2'
+depends 'logstash', '~>0.11'
