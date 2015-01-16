e8c195e7b83e326837602da4dd6f2b42c3ac36a7
Andrew Jo <andrewjo@gmail.com>
2015-01-16 15:40:15 -0800

    Indexer recipe

---
+++ recipes/indexer.rb
@@ -0,0 +1,46 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: opsworks_logstash
+# Recipe:: indexer
+#
+# Copyright (c) 2014, Verdigris Technologies Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+name = "indexer"
+
+logstash_instance name do
+  action :create
+end
+
+logstash_service name do
+  action :enable
+end
+
+logstash_config name do
+  action :create
+  templates_cookbook 'opsworks_logstash'
+  templates ['opsworks_logstash']['indexer']['templates']
+  variables(['opsworks_logstash']['indexer']['template_variables'])
+  notifies :restart, "logstash_service[#{name}]", :delayed
+end
