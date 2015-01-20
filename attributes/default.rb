af0c23d14a466a1bc334d0b07aef838722da84e8
Andrew Jo <andrewjo@gmail.com>
2015-01-19 16:34:08 -0800

    Remove template variables for indexer nodes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,3 @@ default['opsworks_logstash']['indexer']['templates'] = {
   input_rabbitmq: "config/input_rabbitmq.conf.erb",
   output_elasticsearch: "config/output_elasticsearch.conf.erb"
 }
-default['opsworks_logstash']['indexer']['template_variables'] = {
-  input_rabbitmq_host: 'localhost',
-  input_rabbitmq_port: 5672,
-  input_rabbitmq_exchange: 'logstash'
-}
