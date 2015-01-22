a1cb681f1e342f22c04531ff4b5a0214853d13e7
Andrew Jo <andrewjo@gmail.com>
2015-01-21 16:41:47 -0800

    Empty default config_templates_variables for indexer

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,13 +2,9 @@ default['opsworks_logstash']['instance_name'] = 'server'
 
 # Default Logstash Indexer settings
 indexer = default['logstash']['instance']['indexer']
-indexer['config_templates'] = {
-  "indexer.conf" => "config/indexer.conf.erb",
-}
+indexer['config_templates'] = {}
 indexer['config_templates_cookbook'] = 'opsworks_logstash'
-indexer['config_templates_variables'] = {
-  instance_name: "indexer"
-}
+indexer['config_templates_variables'] = { instance_name: "indexer" }
 indexer['inputs'] = []
 indexer['filters'] = []
 indexer['outputs'] = []
